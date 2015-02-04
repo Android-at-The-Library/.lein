3897eed4338d6d181f9f1de50c64b9a8b75695d3
treeherder <b.d.reddybest@gmail.com>
2015-02-04 00:10:30 -0800

    my current working .lein on osx for android development

---
+++ README.md
@@ -0,0 +1,1 @@
+this is a configuration file for leiningen profiles and plugins
---
+++ profiles.clj
@@ -0,0 +1,3 @@
+{:user {:plugins [[lein-exec "0.3.4"] [clodiuno "0.0.4-SNAPSHOT"] [serial-port "1.1.2"] [lein-localrepo "0.5.2"] [lein-droid "0.3.1"] [neko "3.0.0"]
+]}}
+
